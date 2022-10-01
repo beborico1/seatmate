65d52dd23d456dbe7ee2b63bebfad0f1fe4147ef
beborico1 <beborico16@hotmail.com>
2022-10-01 12:38:59 -0500

    added keyword

--- src/pages/Event.js
+++ src/pages/Event.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
+import { useParams } from 'react-router';
+
+
 function Event() {
+  const {eventId} = useParams()
+
   return (
     <div>
-      Event
+      {eventId}
     </div>
   )
 }
