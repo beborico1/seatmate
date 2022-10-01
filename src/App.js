1c754259253b95155a224cfb74aa357749111f88
beborico1 <beborico16@hotmail.com>
2022-10-01 12:41:28 -0500

    bg color

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Search from './pages/Search';
 function App() {
   return (
     <div className="App">
-      <div className='p-5'>
+      <div className='p-5 h-screen bg-gray-100'>
         <Routes>
           <Route strict exact path="/" element={<Search/>}/>
           <Route strict exact path="/:id" element={<Event/>}/>
